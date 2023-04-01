90b0b8078732a69bd649b9d09cece7d1d6a50ac9
Yuri <yuribrasiliano37@gmail.com>
2023-04-01 12:28:18 -0300

    tirando dependencias desnecessárias

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion"
 import Tilt from 'react-tilt'
 import {styles} from '../styles'
+
 import {SectionWrapper} from '../hoc'
 import {fadeIn, textVariant} from '../utils/motion'
 import { languages } from '../constants'
