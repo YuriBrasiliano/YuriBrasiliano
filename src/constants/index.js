f3319c193008787f51918f14499acee5d9c71fa9
Yuri <yuribrasiliano37@gmail.com>
2023-04-02 17:22:52 -0300

    alterando link portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -210,7 +210,7 @@ import {
         },
       ],
       image: portfolio,
-      source_code_link: "https://github.com/",
+      source_code_link: "https://github.com/YuriBrasiliano/YuriBrasiliano",
     },
     {
       name: "Job IT",
